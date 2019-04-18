9d62c8ec0889c9b305e258a66fe17af21dac831b
Silas <silasishallahan@gmail.com>
2019-04-18 12:53:09 -0700

    finished created user

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const xss = require('xss');
+const bcrypt = require('bcryptjs');
+
 const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/;
 
 const UsersService = {
@@ -9,6 +12,14 @@ const UsersService = {
       .where({ user_name })
       .first()
       .then(user => !!user);
+  },
+
+  insertUser(db, newUser) {
+    return db
+      .insert(newUser)
+      .into('thingful_users')
+      .returning('*')
+      .then(([user]) => user);
   },
 
   validatePassword(password) {
@@ -25,7 +36,19 @@ const UsersService = {
       return 'Password must contain 1 upper case, lower case, number and special character';
     }
     return null;
-  }
+  },
+  hashPassword(password) {
+    return bcrypt.hash(password, 12);
+  },
+  serializeUser(user) {
+    return {
+      id: user.id,
+      full_name: xss(user.full_name),
+      user_name: xss(user.user_name),
+      nickname: xss(user.nickname),
+      date_created: new Date(user.date_created),
+    };
+  },
 };
 
 module.exports = UsersService;
--- test/users-endpoints.spec.js
+++ test/users-endpoints.spec.js
@@ -4,6 +4,7 @@
 const knex = require('knex');
 const app = require('../src/app');
 const helpers = require('./test-helpers');
+const bcrypt = require('bcryptjs');
 
 describe.only('Users Endpoints', function() {
   let db;
@@ -124,6 +125,51 @@ describe.only('Users Endpoints', function() {
           .post('/api/users')
           .send(duplicateUser)
           .expect(400, { error: 'Username already taken' });
+      });
+    });
+
+    context('Happy path', () => {
+      it('responds 201, serialized user, storing bcrypted password', () => {
+        const newUser = {
+          user_name: 'test user_name',
+          password: '11AAaa!!',
+          full_name: 'test full_name',
+        };
+
+        return supertest(app)
+          .post('/api/users')
+          .send(newUser)
+          .expect(201)
+          .expect(res => {
+            expect(res.body).to.have.property('id');
+            expect(res.body.user_name).to.eql(newUser.user_name);
+            expect(res.body.full_name).to.eql(newUser.full_name);
+            expect(res.body.nickname).to.eql('');
+            expect(res.body).to.not.have.property('password');
+            expect(res.headers.location).to.eql(`/api/users/${res.body.id}`);
+            const expectedDate = new Date().toLocaleString('en', { timeZone: 'UTC' });
+            const actualDate = new Date(res.body.date_created).toLocaleString();
+            expect(actualDate).to.eql(expectedDate);
+          })
+          .expect(res => {
+            db.from('thingful_users')
+              .select('*')
+              .where({ id: res.body.id })
+              .first()
+              .then(row => {
+                expect(row.user_name).to.eql(newUser.user_name);
+                expect(row.full_name).to.eql(newUser.full_name);
+                expect(row.nickname).to.eql(null);
+                const expectedDate = new Date().toLocaleString('en', { timeZone: 'UTC' });
+                const actualDate = new Date(row.date_created).toLocaleString();
+                expect(actualDate).to.eql(expectedDate);
+
+                return bcrypt.compare(newUser.password, row.password);
+              })
+              .then(compareMatch => {
+                expect(compareMatch).to.be.true;
+              });
+          });
       });
     });
   });
